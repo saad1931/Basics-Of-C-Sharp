e407db95194448bf25868cca7bd3ce2a8200bbfb
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-10-12 22:46:47 +0500

    Updated 1

--- Basics-of-C-Sharp/Program.cs
+++ Basics-of-C-Sharp/Program.cs
@@ -24,6 +24,8 @@ namespace Basics_of_C_Sharp
             Console.WriteLine(Name.Substring(9,2));
             Console.ReadLine();
             */
+            //-----------------------------------------------//
+
 
             //working with numbers
             /*
@@ -38,30 +40,107 @@ namespace Basics_of_C_Sharp
             Console.WriteLine(Math.Max(3,99));
             Console.WriteLine(Math.Round(3.6));
             */
+            //-----------------------------------------------//
+
+
 
             //Getting User Input
+            /*
             Console.Write("Enter Your Name: ");
             string Nm = Console.ReadLine();
             Console.Write("Enter Your Age: ");
             string age = Console.ReadLine();
-            Console.WriteLine("Hello " +Nm+" You are "+age+ " Years Old.");
+            Console.WriteLine("Hello " + Nm + " You are " + age + " Years Old.");
+            */
+            //-----------------------------------------------//
+
 
 
             //Building Basic Calculator
+            /*
             Console.Write("Enter a Number 1: ");
             int num1 =Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter a Number 2: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Addition of "+ num1+" & "+num2+" is equals to " +(num1+num2));
+           */
+            //-----------------------------------------------//
+
+
+
+
+            //Calling Methods
+            //MethodOne();
+            //MethodTwo("Saad");
+            //-----------------------------------------------//
+
+            //Return Statement
+            /*
+            int Square = SquareOfNumber(2);
+            Console.WriteLine(SquareOfNumber(3));
+            Console.WriteLine(Square);
+            */
+            //-----------------------------------------------//
+
+
+            //Madlib Game
+            /*
+            string color, item, name;
+            Console.Write("Enter Color : ");
+            color= Console.ReadLine();
+            Console.Write("Enter Item : ");
+            item = Console.ReadLine();
+            Console.Write("Enter Nmae : ");
+            name = Console.ReadLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Roses are "+color);
+            Console.WriteLine(item+" are blue");
+            Console.WriteLine("I Love "+name);
+            */
+            //-----------------------------------------------//
+
+
+            //Arrays
+            /*
+            int[] RandomNumbers = { 1, 99, 8, 10, 0 };
+            string[] RandomNames = new string[3];
+            RandomNames[0] = "Saad";
+            RandomNames[1] = "Zubair";
+            RandomNames[2] = "Arshad";
+            Console.WriteLine(RandomNames[0]);
+            Console.WriteLine(RandomNumbers[0]);
+            //update array
+            RandomNumbers[0] = 2;
+            Console.WriteLine(RandomNumbers[0]);
+            */
             Console.ReadLine();
 
 
 
 
 
+        }
+
+        //Methods
+        static void MethodOne()
+        {
+            Console.WriteLine("Hi, This is Method One.");
+        }
+
+        //Parameterized Method
+        static void MethodTwo(string Name)
+        {
+            Console.WriteLine("Hi "+Name+", This is Method Two.");
+        }
+        //--------------------------------------------------------//
 
 
+        //Return Statements
+        static int SquareOfNumber(int num)
+        {
+            int result = num * num;
+            return result;
         }
     }
 }
